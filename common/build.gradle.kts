07ccdf3b869a90d375d935dc71d8178542fb837a
Igor Shmagrinskiy <unrealwork@gmail.com>
2023-03-05 23:02:47 +0400

    CI: Setup coverage (#13)
    * #9: base setup for sonarcloud
    
    * #9: fix identation
    
    * #9: rename github secrets
    
    * #9: add status badges
    
    * #12: setup jacoco
    
    * #12: Setup Coverage
    
    * #12: Setup Coverage
    
    * #12: Setup Coverage
    
    * #12: Setup Coverage
    
    * #12: Setup Coverage
    
    * #12: Setup Coverage: test before coverage
    
    * #12: Setup Coverage: test before coverage

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -1,11 +1,20 @@
 plugins {
     java
     checkstyle
+    jacoco
+    alias(libs.plugins.sonar)
 }
 
 dependencies {
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(false)
+    }
 }
 
 
