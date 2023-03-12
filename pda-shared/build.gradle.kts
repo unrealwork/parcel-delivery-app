6d48e29c544ecc456202289dcb72e3e4c62d1c3a
Igor Shmagrinskiy <igor.shmagrinsky@axibase.com>
2023-03-13 00:37:13 +0400

    #12: Setup Coverage: test before coverage

--- pda-shared/build.gradle.kts
+++ pda-shared/build.gradle.kts
@@ -2,19 +2,15 @@ plugins {
     java
     checkstyle
     jacoco
-    alias(libs.plugins.sonar)
+    alias(libs.plugins.spring.boot)
+    alias(libs.plugins.spring.dm)
 }
 
 dependencies {
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
     testImplementation(libs.junit.api)
     testRuntimeOnly(libs.junit.engine)
-}
-
-tasks.jacocoTestReport {
-    reports {
-        xml.required.set(true)
-        html.required.set(false)
-    }
 }
 
 
