cee88639c0874cfd2f9b6d44057d1827f0c7f562
Igor Shmagrinskiy <unrealwork@gmail.com>
2023-03-25 18:40:12 +0400

    #61: Setup API Gateway: proxy, springdoc documentation: tests for MicroserviceOpenApiRegistry

--- pda-api-gateway/src/main/java/parcel/delivery/app/gateway/MicroserviceProperty.java
+++ pda-api-gateway/src/main/java/parcel/delivery/app/gateway/MicroserviceProperty.java
@@ -1,10 +1,12 @@
 package parcel.delivery.app.gateway;
 
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
+@AllArgsConstructor
 public class MicroserviceProperty {
     @NotBlank
     private String host;
