ce631a9695832dd82bcd0029383b4b762de204e8
Igor Shmagrinskiy <unrealwork@gmail.com>
2023-03-25 19:45:40 +0400

    #61: Setup API Gateway: proxy, springdoc documentation: tests for MicroserviceOpenApiRegistry

--- pda-api-gateway/src/test/java/parcel/delivery/app/gateway/config/MicroserviceOpenApiRegistryTest.java
+++ pda-api-gateway/src/test/java/parcel/delivery/app/gateway/config/MicroserviceOpenApiRegistryTest.java
@@ -7,48 +7,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.InjectMocks;
 import org.mockito.Mockito;
-import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import parcel.delivery.app.gateway.MicroserviceProperty;
 
 import static org.hamcrest.CoreMatchers.sameInstance;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.any;
 
 @SpringBootTest
 @ExtendWith( {SpringExtension.class, MockitoExtension.class})
 class MicroserviceOpenApiRegistryTest {
     @Autowired
+    @InjectMocks
     private MicroserviceOpenApiRegistry registry;
-    @SpyBean
+    @MockBean
     private OpenAPIParser swaggerParser;
+    @MockBean
+    private AppProxyConfigurer appProxyConfigurer;
 
-    @Spy
-    private SwaggerParseResult parserResult = new SwaggerParseResult();
-    @Mock
-    private OpenAPI openAPI = new OpenAPI();
 
     @BeforeEach
     void setup() {
-        parserResult.setOpenAPI(openAPI);
-        Mockito.when(swaggerParser.readLocation("http://localhost:8085/v3/api-docs", null, null))
-                .thenReturn(parserResult);
+
 
     }
 
     @Test
     @DisplayName("Test cached registry")
     void restRetrieve() {
+        OpenAPI openAPI = new OpenAPI();
+        SwaggerParseResult parserResult = new SwaggerParseResult();
+        parserResult.setOpenAPI(openAPI);
+        Mockito.when(swaggerParser.readLocation(any(), any(), any()))
+                .thenReturn(parserResult);
         MicroserviceProperty msProperty = new MicroserviceProperty("localhost", 8085);
         OpenAPI registryResult = registry.retrieve(msProperty);
         assertThat(registryResult, equalTo(openAPI));
-        Mockito.when(swaggerParser.readLocation(Mockito.anyString(), Mockito.any(), Mockito.any()))
+        Mockito.when(swaggerParser.readLocation(Mockito.anyString(), any(), any()))
                 .thenReturn(new SwaggerParseResult());
         assertThat(registry.retrieve(msProperty), sameInstance(registryResult));
     }
