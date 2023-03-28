4d7c2cf50ab89f4a46f1f135e3c69d52e7c5ad63
Igor Shmagrinskiy <unrealwork@gmail.com>
2023-03-29 01:44:12 +0400

    #parcel-delivery-app-72: Delivery: delivery-accepted event is sent to the wrong topic (#73)

--- pda-order/src/main/java/parcel/delivery/app/order/messaging/consumer/DeliveryAcceptedConsumer.java
+++ pda-order/src/main/java/parcel/delivery/app/order/messaging/consumer/DeliveryAcceptedConsumer.java
@@ -4,19 +4,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import parcel.delivery.app.common.domain.OrderStatus;
+import parcel.delivery.app.common.messaging.events.DeliveryAcceptedEvent;
 import parcel.delivery.app.order.repository.OrderRepository;
 
-import java.util.UUID;
 import java.util.function.Consumer;
 
 @Component
 @RequiredArgsConstructor
-public class DeliveryAcceptedConsumer implements Consumer<UUID> {
+public class DeliveryAcceptedConsumer implements Consumer<DeliveryAcceptedEvent> {
     private final OrderRepository orderRepository;
 
     @Override
     @Transactional
-    public void accept(UUID uuid) {
-        orderRepository.updateStatus(uuid, OrderStatus.ACCEPTED);
+    public void accept(DeliveryAcceptedEvent event) {
+        orderRepository.updateStatus(event.orderId(), OrderStatus.ACCEPTED);
     }
 }
